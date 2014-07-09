3cf20a1bd4de2a0beb0079a69aee7c2e3a1e80cd
anuradhamandal <anuradhamandal.bd@gmail.com>
2014-07-09 12:29:10 +0600

    Create SharedPrefs.java

---
+++ src/android/SharedPrefs.java
@@ -0,0 +1,67 @@
+package com.preferenceShared.sharedprefs;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import org.apache.cordova.PluginResult;
+import android.content.SharedPreferences;
+
+
+public class SharedPrefs extends CordovaPlugin {
+
+    public SharedPrefs(){
+    }
+
+    public String saveData(JSONArray args, String sharedName, String key, String value){
+        Context context = this.cordova.getActivity();
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedName,context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key,value);
+        editor.commit();
+        return "Done,"+sharedName+","+key+","+value;
+    }
+    public String getData(String sharedName, String key){
+        Context context = this.cordova.getActivity();
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedName,context.MODE_PRIVATE);
+        String data = sharedPreferences.getString(key,null);
+
+        return data;
+    }
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        try {        
+            PluginResult.Status status = PluginResult.Status.OK;
+            String sharedName = args.getString(0);
+            String key = args.getString(1);
+            String value = "";
+            if(args.length()==3){
+                value = args.getString(2);
+            }
+            
+            String result = "";
+            if (action.equals("saveData")) {
+                callbackContext.success(this.saveData(args,sharedName,key,value));
+                return true;
+            }
+            if (action.equals("getData")) {
+                callbackContext.success(this.getData(sharedName,key));
+                return true;
+            } 
+            else {
+                return false;
+            }
+        } catch (JSONException e) {
+            return false;
+        }
+    } 
+}
